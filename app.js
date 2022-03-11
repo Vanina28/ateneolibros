b27d5feb44e8cadd51225f2e1277cf24f9a82138
Vanina28 <vanimeni2002@gmail.com>
2022-03-10 21:40:10 -0300

    vistasejs

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+// Declarando los modulos externos de express
 const express = require('express');
+//const session = require('express-session');
+//const cookies = require('cookie-parser');
+
 const app = express();
 const path = require('path');
-const methodOverride =  require('method-override'); //  métodos PUT y DELETE
+const methodOverride =  require('method-override'); // Para poder usar los métodos PUT y DELETE
 
 //asignando la carpeta public como recurso estatico
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-/* RUTAS */
-const routesMain = require('./routers/mainRouters.js');
-const routesProduct = require('./routers/productRouters.js');
-const routesUsers = require('./routers/usersRouters.js');
-const routesAdmin = require('./routers/adminRouters.js');
+/**********Rutas*************/
+//const routesMain = require('./routers/mainRouters.js');
+//const routesProduct = require('./routers/productRouters.js');
+//const routesUsers = require('./routers/usersRouters.js');
+//const routesAdmin = require('./routers/adminRouters.js');
 
 
-/* MIDDLEWARES */
+/*************Middlewares**************/
 
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 app.use(express.urlencoded({extended : false})); //recibir los valores de un formulario por POST
@@ -23,37 +27,39 @@ app.use(express.urlencoded({extended : false})); //recibir los valores de un for
 app.set('view engine','ejs');
 
 /**************** Middleware de aplicacion ******************/
-const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+//const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 
-app.use(session({
-    secret: "Shhh, It's a secret",
-    resave: false,
-    saveUninitialized: false,
-}))//para generar sessiones en el sistema
+//app.use(session({
+  //  secret: "Shhh, It's a secret",
+    //resave: false,
+    //saveUninitialized: false,
+//}))//para generar sessiones en el sistema
 
 
 //Utilizando Cookies
-app.use(cookies());
+//app.use(cookies());
 
 //Utilizando session
-app.use(userLoggedMiddleware);
+//app.use(userLoggedMiddleware);
 
 
 // Asignando el servidor
-app.listen(3030, () => {
-    console.log("Servidor ejecutandose en el puerto 3030");
+app.listen(5000, () => {
+    console.log("Servidor ejecutandose en el puerto 5000");
 });
 
 // Declarando la ruta del archivo inicio
-app.use('/', routesMain);
+//app.use('/', routesMain);
 
 // Declarando la ruta del archivo detalle del producto y carrito de compras
-app.use('/products', routesProduct);
+//app.use('/products', routesProduct);
 
 // Declarando la ruta del archivo registro1 
-app.use('/users', routesUsers);
+//app.use('/users', routesUsers);
 
 // Declarando la ruta del archivo login
-app.use('/admin', routesAdmin);
+//app.use('/admin', routesAdmin);
+
+
 
 
