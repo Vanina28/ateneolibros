1b187b2dae25f8f6c466c1cc2f78043de5384cec
Vanina28 <vanimeni2002@gmail.com>
2022-03-14 10:13:55 -0300

    controladores

---
+++ controllers/adminController.js
@@ -0,0 +1,276 @@
+// ************ Require's ************
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const {validationResult} = require('express-validator');
+// base de datos 
+const db = require("../database/models");
+
+//asignando la carpeta public como recurso estatico
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+/*********Conectando Json***********/
+//Productos
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+//Usuarios
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+
+let adminController = {
+  //Index - Mostrar el administrador de productos
+  
+  index: async (req,res) => {
+    let productsDB = await db.Productos.findAll();
+    let categorias = await db.Categorias.findAll();
+    let autores = await db.Autores.findAll();
+
+    res.render('./admin/manageProducts',{
+      dataBooks: productsDB,
+      categorias,
+      autores,
+      user: req.session.userLogged
+    });
+  },
+   // Añadir - formulario de crear
+  add: async (req, res) => {
+    let categorias = await db.Categorias.findAll();
+    let autores = await db.Autores.findAll();
+    let formatos = await db.Formatos.findAll();
+
+    res.render('./admin/addProduct',{
+      categorias,
+      autores,
+      formatos
+    });
+  },
+  // Crear -  Metodo de crear en la tienda
+  store: async (req, res) => {
+    let resultValidation = validationResult(req);
+    if(resultValidation.errors.length > 0){
+      let categorias = await db.Categorias.findAll();
+      let autores = await db.Autores.findAll();
+      let formatos = await db.Formatos.findAll();
+      res.render('./admin/addProduct',{        
+        categorias,
+        autores,
+        formatos,
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+        user: req.session.userLogged
+      });
+
+    }else{
+      const {nameBook,author,price,publisher, format, category, sku, language,edition, pages,chapters,description} = req.body;
+
+      db.Productos.create({
+          sku,
+          nameBook, 
+          idAuthor:author,
+          price,
+          publisher,
+          idFormat:format,
+          idCategory:category,
+          languageBook:language,
+          editionBook:edition,
+          pages,
+          chapters,
+          descriptionBook:description,
+          imageProduct: req.file.filename
+      });  
+
+      let productsDB = await db.Productos.findAll();
+      let categorias = await db.Categorias.findAll();
+      let autores = await db.Autores.findAll();
+
+      res.render('./admin/manageProducts',{
+        dataBooks: productsDB,
+        categorias,
+        autores,
+        user: req.session.userLogged
+      });
+      
+    }
+    				
+  },
+
+  // Edit - formulario de editar
+  edit: async (req, res) => {
+    const idProduct = req.params.id;
+
+		const productEdit = await db.Productos.findByPk(idProduct);
+
+    let categorias = await db.Categorias.findAll();
+    let autores = await db.Autores.findAll();
+    let formatos = await db.Formatos.findAll();
+
+    res.render('./admin/editProduct',{ 
+    product : productEdit, 
+    categorias, 
+    autores, 
+    formatos
+  });
+    	
+  },
+  // Actualizar - método de actualizar
+  update: async (req, res) => {
+    let resultValidation = validationResult(req);
+    const idProduct = req.params.id;
+    const productEdit = await db.Productos.findByPk(idProduct);
+
+    if(resultValidation.isEmpty()){
+      const {nameBook,author,price,publisher, format, category, sku, language,edition, pages,chapters,description,image} = req.body;
+
+      const fileNameBook = (req.file) ? req.file.filename : image;
+    
+      db.Productos.update({
+          sku,
+          nameBook, 
+          idAuthor:author,
+          price,
+          publisher,
+          idFormat:format,
+          idCategory:category,
+          languageBook:language,
+          editionBook:edition,
+          pages,
+          chapters,
+          descriptionBook:description,
+          imageProduct: fileNameBook
+      },{
+        where:{
+          sku: req.params.id
+        }
+      });  
+
+      let productsDB = await db.Productos.findAll();
+      let categorias = await db.Categorias.findAll();
+      let autores = await db.Autores.findAll();
+      
+      res.render('./admin/manageProducts',{
+        dataBooks: productsDB,
+        categorias,
+        autores,
+        user: req.session.userLogged
+      });
+    }else{
+      let categorias = await db.Categorias.findAll();
+      let autores = await db.Autores.findAll();
+      let formatos = await db.Formatos.findAll();
+      res.render('./admin/editProduct',{     
+        categorias,
+        autores,
+        formatos,
+        product : productEdit,
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+        user: req.session.userLogged
+      });
+    }
+  },
+  // Eliminar - Formulario de confirmar eliminado
+  delete: async (req, res) => {
+    let idProduct = req.params.id;
+
+    let productDelete = await db.Productos.findByPk(idProduct);
+
+    res.render('./admin/deleteProduct',{productDelete});
+  },
+  // Borrar - Eliminar un producto de la BD
+  destroy: async (req, res) => {
+    let idBook = req.params.id;
+		
+    db.Productos.destroy({
+      where: {
+        sku: idBook
+      }
+  })
+    let productsDB = await db.Productos.findAll();
+    let categorias = await db.Categorias.findAll();
+    let autores = await db.Autores.findAll();
+
+		res.render('./admin/manageProducts',{
+      dataBooks: productsDB,
+      categorias,
+      autores,
+      user: req.session.userLogged
+    });		
+  },
+  // Clientes - Mostrar todos los clientes
+  customers: (req, res) => {    
+		res.render('./admin/customers',{
+    dataUsers: users,
+    user: req.session.userLogged});
+  },
+  // Edit - formulario de editar cliente
+  editCustomers:(req, res) => {
+    let idCustomer = req.params.id;
+		customerEdit = users.find(item => item.id == idCustomer);
+    res.render('./admin/editCustomer',{ customer : customerEdit });
+  },
+  // Actualizar - método de actualizar
+  updateCustomer: (req, res) => {
+    let resultValidation = validationResult(req);
+    if(resultValidation.isEmpty()){
+      const {name,lastname,email,phone,city,pass,confirmpass,role,image} = req.body;
+		const idCustomer = req.params.id;
+    const fileNameCustomer = (req.file) ? req.file.filename : image;
+    const customersNews = [];
+    users.map(item =>{
+			if(item.id == idCustomer){
+				item.name_user = name; 
+        item.last_name = lastname;
+        item.email = email; 
+        item.city = city;
+        item.phone = phone;
+        item.password = pass;
+        item.confirmpass = confirmpass;        
+        item.image = fileNameCustomer;
+        item.role = role;        
+        customersNews.push(item);
+			}else{
+        customersNews.push(item);
+      }			
+		});		
+		fs.writeFileSync(usersFilePath,JSON.stringify(customersNews),'utf-8');
+		res.render('./admin/customers',{dataUsers: customersNews});	
+    }else{
+      res.render('./admin/editCustomer',{
+        customer : customerEdit,
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+        user: req.session.userLogged
+      });
+    }    	
+  },
+  // Eliminar - Formulario de confirmar eliminado cliente
+  deleteCustomer: (req, res) => {
+    let idCustomer = req.params.id;
+		customerDelete = users.find(item => item.id == idCustomer);
+    return res.render('./admin/deleteCustomer',{
+      customerDelete,
+      user: req.session.userLogged
+    });
+  },
+  // Borrar - Eliminar un cliente de la BD
+  destroyCustomer: (req, res) => {
+    let idCustomer = req.params.id;
+		const customersNews = [];
+		users.map(item =>{
+			if(item.id != idCustomer){
+				customersNews.push(item);
+			}			
+		});		
+		fs.writeFileSync(usersFilePath,JSON.stringify(customersNews),'utf-8');
+		res.render('./admin/customers',{
+      dataUsers: customersNews,
+      user: req.session.userLogged
+    });		
+  },
+}
+
+module.exports = adminController;
---
+++ controllers/mainController.js
@@ -0,0 +1,20 @@
+// Declarando los modulos externos de express
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+//asignando la carpeta public como recurso estatico
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+let mainController = {
+  index: function(req,res){
+    res.render('./main/index', {dataBooks:products});
+  }
+}
+
+module.exports = mainController;
