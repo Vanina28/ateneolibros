80b54fc4123098d27e5c4f34eae76b4100eccbb3
Vanina28 <vanimeni2002@gmail.com>
2022-03-24 19:29:12 -0300

    data

---
+++ data/images.sql
@@ -0,0 +1,8 @@
+INSERT INTO images VALUES
+("1", "etdoloredignissimos.png"),
+("2", "exmodialiquam.png"),
+("3", "suscipitsuntcorporis.png"),
+("4", "molestiaedoloresipsa.png"),
+("5", "istequaspossimus.png"),
+("6", "nemosimiliquequod.png"),
+("7", "utdoloresanimi.png");
---
+++ data/users.sql
@@ -0,0 +1,8 @@
+IdUsersINSERT INTO users VALUES
+( "1","ldumsday0@ameblo.jp", "Laurice","$2a$10$7ph.0", "$2a$10$7ph.0","1"),
+("2", "gscola1@senate.gov", "Gena", "$2a$10$7ph.0", "$2a$10$7ph.0","2"),
+("3","rcowherd2@soundcloud.com", "Roderigo","$2a$10$7ph.0","$2a$10$7ph.0","3"),
+("4", "lwiskar3@unc.edu","Ludovika","$2a$10$7ph.0", "$2a$10$7ph.0","4"),
+("5","vbushaway4@slate.com","Valina", "$2a$10$7ph.0", "$2a$10$7ph.0","5"),
+("6","kbruck5@cnbc.com","Kyle","$2a$10$7ph.0", "$2a$10$7ph.0","6"),
+("7", "eiuorio6@purevolume.com","Emmey","$2a$10$7ph.0", "$2a$10$7ph.0", "7");
